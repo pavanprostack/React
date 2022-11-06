91712c04c66793c33a5826b39884966f4d28eb3d
pavanprostack <pavanprostack@gmail.com>
2022-11-06 13:31:39 +0530

    okkk

---
+++ Product-CRUD-Apps/navigationapp/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './Navbar/Navbar'
+import CreateProduct from './Product/CreateProduct'
+import ProductList from './Product/ProductList'
+import Home from './Product/Home'
+import Admin from '../src/Product/Admin'
+
+
+const App = () => {
+    return <>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path='/home' element={<Home />} />
+                <Route path='/product' element={<CreateProduct />} />
+                <Route path='/list' element={<ProductList />} />
+                <Route path='admin' element={<Admin/>} />
+            </Routes>
+        </Router>
+    </>
+
+}
+
+export default App
---
+++ Product-CRUD-Apps/navigationapp/src/Product/Admin.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Axios from 'axios'
+
+const Admin = () => {
+    let [products, setProducts]=useState([])
+    let [errorMessage, setErrorMessage]= useState('')
+    useEffect(()=>{
+        Axios.get('http://127.0.0.1:5000/api/products/').then((response)=>{
+            setProducts(response.data)
+        }).catch((err)=>{
+            setErrorMessage(err)
+        })
+    }, [])
+  return <>
+  <div className="container">
+    <div className="row">
+        <pre>{JSON.stringify(products)}</pre>
+    <div className="col-6">
+        <table className='table table-hover'>
+            <thead className='bg-primary text-white'>
+                <tr>
+                    <th>Name</th>
+                    <th>Image</th>
+                    <th>Price</th>
+                    <th>Qty</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+               {
+                products.length>0?<>
+                {
+                    products.map((product, index)=>{
+                        return <tr key={index}>
+                            <td>{product.name}</td>
+                            <td><img src={product.image} alt="img" width='70px'/></td>
+                            <td>{product.name}</td>
+                            <td>{product.qty}</td>
+                        </tr>
+                    })
+                }
+                </> : null
+               }
+            </tbody>
+        </table>
+    </div>
+    </div>
+  </div>
+  </>
+}
+
+export default Admin
