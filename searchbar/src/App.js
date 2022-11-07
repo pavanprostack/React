812609a6a07fdc36b8a724b97de4e49fd24bf4f5
pavanprostack <pavanprostack@gmail.com>
2022-11-07 23:06:09 +0530

    search

---
+++ searchbar/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search from './Components/Search'
+import Search1 from './Components/Search1'
+
+const App = () => {
+  return <>
+  <Search />
+  <Search1 />
+  </>
+}
+
+export default App
